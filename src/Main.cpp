f4abab13ede5852731b542d5f2889f38ad0ac56e
Alexandru Manoila <alexmanoila06@gmail.com>
2026-03-03 17:29:13 +0200

    Add more rotating boxes

--- src/Main.cpp
+++ src/Main.cpp
@@ -138,6 +138,18 @@ int main() {
     glm::mat4 view = glm::mat4(1.0f);
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
     glm::mat4 projection = glm::perspective(glm::radians(45.0f), 16.0f / 10.0f, 0.1f, 100.0f);
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
 
     // shader creating
     // ---------------
@@ -148,6 +160,7 @@ int main() {
     shader.setMat4("model", model);
     shader.setMat4("view", view);
     shader.setMat4("projection", projection);
+
 
     // render loop
     // ----------
@@ -163,14 +176,19 @@ int main() {
         // ----------------------
         float time = glfwGetTime();
         model = glm::rotate(glm::mat4(1.0f), glm::radians(time) * 50.0f, glm::vec3(0.5f, 1.0f, 0.0f));
-        shader.setMat4("model", model);
 
         // rendering commands
         // --------------------
         glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+        for (int i = 0; i < 10; i++) {
+            model = glm::translate(glm::mat4(1.0f), cubePositions[i]);
+            model = glm::rotate(model, time, glm::vec3(time * i, time, 0.0f));
+            shader.setMat4("model", model);
+            glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        }
 
         // check and call events / buffer swaping
         // ---------------------------------------
